fded9f3b15a4994fe4ce3d3c0a8f4fe7567c8638
AuroraJunco <aurorajunco@outlook.com>
2024-04-26 18:24:45 +0200

    enviar

--- .github/pfinal/p5/src/main/java/edu/comillas/icai/gitt/pat/spring/p5/model/ProfileResponse.java
+++
@@ -1,9 +0,0 @@
-package edu.comillas.icai.gitt.pat.spring.p5.model;
-
-import edu.comillas.icai.gitt.pat.spring.p5.entity.AppUser;
-
-public record ProfileResponse(
-    String name,
-    String email,
-    Role role
-) { }
--- .github/pfinal/p5/src/main/java/edu/comillas/icai/gitt/pat/spring/p5/model/Role.java
+++
@@ -1,5 +0,0 @@
-package edu.comillas.icai.gitt.pat.spring.p5.model;
-
-public enum Role {
-    USER, ADMIN
-}
