680bd4372b583408d3b3a85a35e7f49086da0e94
allendeyarza <allendedeyarza@gmail.com>
2024-05-15 23:59:22 +0200

    apuntarseClase modificaciones

--- jpa/src/main/resources/static/app.js
+++ jpa/src/main/resources/static/app.js
@@ -1,4 +1,4 @@
-function datosPerfil() {
+/*function datosPerfil() {
   return fetch('/api/users/me').then(res => res.json());
 }
 
@@ -47,4 +47,32 @@ function form2json(event) {
   event.preventDefault();
   const data = new FormData(event.target);
   return JSON.stringify(Object.fromEntries(data.entries()));
-}
+}*/
+document.addEventListener('DOMContentLoaded', function() {
+  let slides = document.getElementsByClassName("review");
+  let prevButton = document.querySelector('.prev');
+  let nextButton = document.querySelector('.next');
+
+  let slideIndex = 0;
+  showSlides(slideIndex); // Muestra el primer slide
+
+  prevButton.addEventListener('click', function() { moveSlide(-1); });
+  nextButton.addEventListener('click', function() { moveSlide(1); });
+
+  function moveSlide(n) {
+    slideIndex += n;
+    if (slideIndex >= slides.length) {
+      slideIndex = 0;
+    } else if (slideIndex < 0) {
+      slideIndex = slides.length - 1;
+    }
+    showSlides(slideIndex);
+  }
+
+  function showSlides(n) {
+    for (let i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+    }
+    slides[n].style.display = "block";
+  }
+});
